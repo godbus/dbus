206e053808f264d2f29356c0987474e502f5d449
Georg Reinke <guelfey@googlemail.com>
2013-02-05 22:51:05 +0100

    Encoder: refuse values that are nested too deeply
    Fixes #9. (Really.)

--- encoder.go
+++ encoder.go
@@ -56,7 +56,7 @@ func (enc *Encoder) Encode(v interface{}) (err error) {
 			}
 		}
 	}()
-	enc.encode(reflect.ValueOf(v))
+	enc.encode(reflect.ValueOf(v), 0)
 	return nil
 }
 
@@ -70,8 +70,9 @@ func (enc *Encoder) EncodeMulti(vs ...interface{}) error {
 	return nil
 }
 
-// encode encodes the given value to the writer and panics on error.
-func (enc *Encoder) encode(v reflect.Value) {
+// encode encodes the given value to the writer and panics on error. depth holds
+// the depth of the container nesting.
+func (enc *Encoder) encode(v reflect.Value, depth int) {
 	enc.align(alignment(v.Type()))
 	switch v.Kind() {
 	case reflect.Uint8:
@@ -81,9 +82,9 @@ func (enc *Encoder) encode(v reflect.Value) {
 		enc.pos++
 	case reflect.Bool:
 		if v.Bool() {
-			enc.encode(reflect.ValueOf(uint32(1)))
+			enc.encode(reflect.ValueOf(uint32(1)), depth)
 		} else {
-			enc.encode(reflect.ValueOf(uint32(0)))
+			enc.encode(reflect.ValueOf(uint32(0)), depth)
 		}
 	case reflect.Int16:
 		enc.binwrite(int16(v.Int()))
@@ -107,22 +108,25 @@ func (enc *Encoder) encode(v reflect.Value) {
 		enc.binwrite(v.Float())
 		enc.pos += 8
 	case reflect.String:
-		enc.encode(reflect.ValueOf(uint32(len(v.String()))))
+		enc.encode(reflect.ValueOf(uint32(len(v.String()))), depth)
 		n, err := enc.out.Write([]byte(v.String() + "\x00"))
 		if err != nil {
 			panic(err)
 		}
 		enc.pos += n
 	case reflect.Ptr:
-		enc.encode(v.Elem())
+		enc.encode(v.Elem(), depth)
 	case reflect.Slice, reflect.Array:
+		if depth >= 64 {
+			panic(FormatError("input exceeds container depth limit"))
+		}
 		buf := new(bytes.Buffer)
 		bufenc := NewEncoder(buf, enc.order)
 
 		for i := 0; i < v.Len(); i++ {
-			bufenc.encode(v.Index(i))
+			bufenc.encode(v.Index(i), depth+1)
 		}
-		enc.encode(reflect.ValueOf(uint32(buf.Len())))
+		enc.encode(reflect.ValueOf(uint32(buf.Len())), depth)
 		length := buf.Len()
 		enc.align(alignment(v.Type().Elem()))
 		if _, err := buf.WriteTo(enc.out); err != nil {
@@ -130,10 +134,13 @@ func (enc *Encoder) encode(v reflect.Value) {
 		}
 		enc.pos += length
 	case reflect.Struct:
+		if depth >= 64 && v.Type() != signatureType {
+			panic(FormatError("input exceeds container depth limit"))
+		}
 		switch t := v.Type(); t {
 		case signatureType:
 			str := v.Field(0)
-			enc.encode(reflect.ValueOf(byte(str.Len())))
+			enc.encode(reflect.ValueOf(byte(str.Len())), depth+1)
 			n, err := enc.out.Write([]byte(str.String() + "\x00"))
 			if err != nil {
 				panic(err)
@@ -141,19 +148,24 @@ func (enc *Encoder) encode(v reflect.Value) {
 			enc.pos += n
 		case variantType:
 			variant := v.Interface().(Variant)
-			enc.encode(reflect.ValueOf(variant.sig))
-			enc.encode(reflect.ValueOf(variant.value))
+			enc.encode(reflect.ValueOf(variant.sig), depth+1)
+			enc.encode(reflect.ValueOf(variant.value), depth+1)
 		default:
 			for i := 0; i < v.Type().NumField(); i++ {
 				field := t.Field(i)
 				if unicode.IsUpper([]rune(field.Name)[0]) &&
 					field.Tag.Get("dbus") != "-" {
 
-					enc.encode(v.Field(i))
+					enc.encode(v.Field(i), depth+1)
 				}
 			}
 		}
 	case reflect.Map:
+		// Maps are arrays of structures, so they actually increase the depth by
+		// 2.
+		if depth >= 63 {
+			panic(FormatError("input exceeds container depth limit"))
+		}
 		if !isKeyType(v.Type().Key()) {
 			panic(invalidTypeError{v.Type()})
 		}
@@ -162,10 +174,10 @@ func (enc *Encoder) encode(v reflect.Value) {
 		bufenc := NewEncoder(buf, enc.order)
 		for _, k := range keys {
 			bufenc.align(8)
-			bufenc.encode(k)
-			bufenc.encode(v.MapIndex(k))
+			bufenc.encode(k, depth+2)
+			bufenc.encode(v.MapIndex(k), depth+2)
 		}
-		enc.encode(reflect.ValueOf(uint32(buf.Len())))
+		enc.encode(reflect.ValueOf(uint32(buf.Len())), depth)
 		length := buf.Len()
 		enc.align(8)
 		if _, err := buf.WriteTo(enc.out); err != nil {
