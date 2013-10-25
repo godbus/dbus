a8f74ea1d91713f83eb4a25d27870d897c028878
Jesse van den Kieboom <jessevdk@gmail.com>
2013-10-25 16:57:17 +0200

    Use launchctl to obtain session bus address on OS X
    The "standard" way to launch dbus on OS X is through launchd which
    is responsible for allocating a unix domain socket and then passing
    it to dbus. This patch splits out a platform specific method to obtain
    the session bus if DBUS_SESSION_BUS_ADDRESS is not set. On OS X
    launchctl is queried while on other platforms dbus-launch is used
    as before.

---
+++ conn_other.go
@@ -0,0 +1,26 @@
+// +build !darwin
+
+package dbus
+
+import (
+	"bytes"
+	"os/exec"
+)
+
+func SessionBusPlatform() (*Conn, error) {
+	cmd := exec.Command("dbus-launch")
+	b, err := cmd.CombinedOutput()
+
+	if err != nil {
+		return nil, err
+	}
+
+	i := bytes.IndexByte(b, '=')
+	j := bytes.IndexByte(b, '\n')
+
+	if i == -1 || j == -1 {
+		return nil, errors.New("dbus: couldn't determine address of session bus")
+	}
+
+	return Dial(string(b[i+1 : j]))
+}
