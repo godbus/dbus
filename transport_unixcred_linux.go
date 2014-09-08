68003e5e7aa7730552caff6670d24d82f9e81b0b
Varialus <varialus@gmail.com>
2014-09-07 21:21:30 -0600

    Add UnixCredentials system call for DragonFly BSD

--- transport_unixcred_linux.go
+++ transport_unixcred_linux.go
@@ -1,4 +1,7 @@
-// +build !darwin,!windows
+// The UnixCredentials system call is currently only implemented on Linux
+// http://golang.org/src/pkg/syscall/sockcmsg_linux.go
+// https://groups.google.com/forum/#!topic/golang-dev/z7VbyqR1s78
+// http://code.google.com/p/go/source/browse/unix/sockcmsg_linux.go?repo=sys
 
 package dbus
 
