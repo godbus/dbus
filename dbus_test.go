2a393b3dac834c674d39a5eeb835f7426b900800
赵鹏 <zhaopeng@uniontech.com>
2021-11-10 13:25:26 +0800

    feat: add variant stroe some case unit test
    when we use dbus to communicate, variant maybe lost
    data type info, because args was transform to bytes.
    so in unit test,first encode message ,then decode bytes.
    (it simulate one communicate).

--- dbus_test.go
+++ dbus_test.go
@@ -1,6 +1,8 @@
 package dbus
 
 import (
+	"bytes"
+	"reflect"
 	"testing"
 )
 
@@ -20,5 +22,36 @@ func Test_VariantOfStruct(t *testing.T) {
 	}
 	if tester != output {
 		t.Fatalf("%v != %v\n", tester, output)
+	}
+}
+
+func Test_VariantOfSlicePtr(t *testing.T) {
+	value := []TestStruct{{1, "1"}}
+	dest := []*TestStruct{}
+
+	parm := &Message{
+		Type:  TypeMethodCall,
+		Flags: FlagNoAutoStart,
+		Headers: map[HeaderField]Variant{
+			FieldPath:        MakeVariant(ObjectPath("/example")),
+			FieldDestination: MakeVariant(""),
+			FieldMember:      MakeVariant("call"),
+		},
+		Body: []interface{}{value},
+	}
+	parm.Headers[FieldSignature] = MakeVariant(SignatureOf(parm.Body...))
+	buf := new(bytes.Buffer)
+	err := parm.EncodeTo(buf, nativeEndian)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	msg, err := DecodeMessage(buf)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := Store(msg.Body, &dest); err != nil || reflect.DeepEqual(value, dest) {
+		t.Log(err)
+		t.FailNow()
 	}
 }
