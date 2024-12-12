ae878aff425af744e95b5147c09b7a01642421d6
anonymix007 <48598263+anonymix007@users.noreply.github.com>
2024-12-12 14:34:36 +0300

    transport_unix: add newUnixTransportFromConn

--- transport_unix.go
+++ transport_unix.go
@@ -55,6 +55,14 @@ type unixTransport struct {
 	hasUnixFDs bool
 }
 
+func newUnixTransportFromConn(conn *net.UnixConn) (transport, error) {
+	t := new(unixTransport)
+	t.UnixConn = conn
+	t.hasUnixFDs = true
+
+	return t, nil
+}
+
 func newUnixTransport(keys string) (transport, error) {
 	var err error
 
