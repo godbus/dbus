d03c0bea70755580de832141a43f544950e76fc7
Kir Kolyshkin <kolyshkin@gmail.com>
2025-12-02 14:14:59 -0800

    Use switch where it makes sense
    As recommended by staticcheck linter, e.g.
    
    > conn.go:448:3: QF1003: could use tagged switch on member (staticcheck)
    
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

--- sig.go
+++ sig.go
@@ -89,9 +89,10 @@ func getSignature(t reflect.Type, depth *depthCounter) (sig string) {
 		}
 		return "s"
 	case reflect.Struct:
-		if t == variantType {
+		switch t {
+		case variantType:
 			return "v"
-		} else if t == signatureType {
+		case signatureType:
 			return "g"
 		}
 		var s string
@@ -258,9 +259,10 @@ func validSingle(s string, depth *depthCounter) (err error, rem string) {
 func findMatching(s string, left, right rune) int {
 	n := 0
 	for i, v := range s {
-		if v == left {
+		switch v {
+		case left:
 			n++
-		} else if v == right {
+		case right:
 			n--
 		}
 		if n == 0 {
